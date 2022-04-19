9dbd0122be2dc0ba2f4d2ba652e0ab97d4c300b8
ptrckmrgn <patrick.morgan@live.com.au>
2022-04-19 20:52:06 +1000

    Updated to support varients

--- src/plugin.ts
+++ src/plugin.ts
@@ -6,31 +6,39 @@ const plugin = () => {
 
     if (selection.length > 0) {
         for (const node of selection) {
-            components = components.concat(getComponentNodes(node));
+            components = components.concat(getComponentNodes(node));            
         }
         for (const component of components) {
+            console.log(component);
+
             const parent = component.parent;
             const frame = createFrameFromComponent(component);
-            // Rename frame and reposition in tree and on the canvas
+            // Reposition frame in tree and on the canvas
             if (parent) {
-                const index = parent.children.indexOf(component);
-                parent.insertChild(index, frame);
+                if (parent.type !== "COMPONENT_SET") {
+                    const index = parent.children.indexOf(component);
+                    parent.insertChild(index, frame);
+                    frame.x = component.x;
+                    frame.y = component.y;
+                }
+                else { // It's a set of variants
+                    const grandparent = parent.parent;
+                    if (grandparent) {
+                        const index = grandparent.children.indexOf(parent);
+                        grandparent.insertChild(index, frame);
+                    }
+                    frame.x = parent.x + component.x;
+                    frame.y = parent.y + component.y;
+                    frame.name = component.name; // Rename to capture variant properties
+                }
             }
-            // frame.name = component.name;
-            frame.x = component.x;
-            frame.y = component.y;
+            else {
+                frame.x = component.x;
+                frame.y = component.y;
+            }
 
             // Save layer tree expanded state
             frame.setPluginData("expanded", JSON.stringify(component.expanded));
-
-            // // Clone and position all the component children in the new frame
-            // for (const child of component.children) {
-            //     const newChild = child.clone();
-            //     frame.appendChild(newChild);
-            //     newChild.x = child.x;
-            //     newChild.y = child.y;
-            // }
-
             frame.expanded = component.expanded;
             component.remove();
             frames.push(frame);
@@ -47,7 +55,7 @@ const plugin = () => {
             }
 
             figma.notify(
-                `${count} main component${
+                `${count} component${
                     count > 1 ? "s" : ""
                 } successfully detached`
             );
@@ -77,68 +85,6 @@ const getComponentNodes = (node: SceneNode): ComponentNode[] => {
 const createFrameFromComponent = (component: ComponentNode): FrameNode => {
     const instance = component.createInstance();
     const frame = instance.detachInstance();
-
-    // // Layout-related properties (excluding position)
-    // frame.rotation = component.rotation;
-    // frame.resize(component.width, component.height);
-    // frame.layoutAlign = component.layoutAlign;
-    // frame.constraints = component.constraints;
-
-    // // Frame properties
-    // frame.clipsContent = component.clipsContent;
-    // frame.guides = clone(component.guides);
-    // frame.layoutGrids = clone(component.layoutGrids);
-    // frame.gridStyleId = component.gridStyleId;
-    // frame.layoutMode = component.layoutMode;
-    // frame.counterAxisSizingMode = component.counterAxisSizingMode;
-    // frame.horizontalPadding = component.horizontalPadding;
-    // frame.verticalPadding = component.verticalPadding;
-    // frame.itemSpacing = component.itemSpacing;
-
-    // // Container-related properties
-    // frame.expanded = component.expanded;
-    // frame.backgrounds = clone(component.backgrounds);
-    // frame.backgroundStyleId = component.backgroundStyleId;
-
-    // // Geometry-related properties
-    // frame.fills = clone(component.fills);
-    // frame.strokes = clone(component.strokes);
-    // frame.expanded = component.expanded;
-    // frame.strokeWeight = component.strokeWeight;
-    // frame.strokeMiterLimit = component.strokeMiterLimit;
-    // frame.strokeAlign = component.strokeAlign;
-    // frame.strokeCap = component.strokeCap;
-    // frame.strokeJoin = component.strokeJoin;
-    // frame.dashPattern = component.dashPattern;
-    // frame.fillStyleId = component.fillStyleId;
-    // frame.strokeStyleId = component.strokeStyleId;
-    // frame.fillStyleId = component.fillStyleId;
-
-    // // Corner-related properties
-    // frame.cornerSmoothing = component.cornerSmoothing;
-    // if (component.cornerRadius === figma.mixed) {
-    //     frame.topLeftRadius = component.topLeftRadius;
-    //     frame.topRightRadius = component.topRightRadius;
-    //     frame.bottomLeftRadius = component.bottomLeftRadius;
-    //     frame.bottomRightRadius = component.bottomRightRadius;
-    // } else {
-    //     frame.cornerRadius = component.cornerRadius;
-    // }
-
-    // // Blend-related properties
-    // frame.opacity = component.opacity;
-    // frame.blendMode = component.blendMode;
-    // frame.isMask = component.isMask;
-    // frame.effects = clone(component.effects);
-    // frame.effectStyleId = component.effectStyleId;
-
-    // // Export-related properties
-    // frame.exportSettings = clone(component.exportSettings);
-
-    // // Frame prototyping-related properties
-    // frame.overflowDirection = component.overflowDirection;
-    // frame.numberOfFixedChildren = component.numberOfFixedChildren;
-
     return frame;
 };
 
